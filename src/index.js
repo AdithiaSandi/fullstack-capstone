26120ba74f8ed87d51d42f33860755bb54e30cb8
Adithia Sandi Josua Simangunsong <106661816+AdithiaSandi@users.noreply.github.com>
2023-01-12 18:03:45 +0700

    add order route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import userRoute from "./users/users.routes.js";
 import authRoute from "./auth/auth.routes.js";
 import addressRoute from "./address/address.routes.js";
+import orderRoute from "./order/order.routes.js"
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", userRoute);
 app.use("/api/v1", authRoute);
 app.use("/api/v1", addressRoute);
+app.use("/api/v1", orderRoute)
 
 // start server
 app.listen(process.env.API_PORT, () => {
